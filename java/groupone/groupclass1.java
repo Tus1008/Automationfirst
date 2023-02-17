b54c91838870c8c4a1f3eeb420998c612bfcc140
tushar panchani <panchani1008@gmail.com>
2023-02-17 21:33:52 +0000

    chirag

--- java/groupone/groupclass1.java
+++ java/groupone/groupclass1.java
@@ -3,6 +3,8 @@ package groupone;
 public class groupclass1 {
     public static void main(String[] args){
         System.out.println("automation first");
+        System.out.println("Chirag");
+        System.out.println("automation class java");
     }
 }
 
